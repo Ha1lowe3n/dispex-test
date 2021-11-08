978f9dd510c89c2014547fb9c1b3ebeaf6aefe5a
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-11-08 12:08:43 +0800

    add buttons, refactor styles

--- src/components/ResidentsList/index.jsx
+++ src/components/ResidentsList/index.jsx
@@ -4,6 +4,10 @@ import { useSelector } from "react-redux";
 import PersonIcon from "@mui/icons-material/Person";
 import CallIcon from "@mui/icons-material/Call";
 import EmailIcon from "@mui/icons-material/Email";
+import Button from "@mui/material/Button";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CreateIcon from "@mui/icons-material/Create";
 import styles from "./ResidentsList.module.scss";
 
 export const ResidentsList = () => {
@@ -12,42 +16,61 @@ export const ResidentsList = () => {
     console.log(fullAddressTitle, residents);
     return (
         <>
-            <h3>{fullAddressTitle}</h3>
-            <div className={styles.list}>
-                {residents.clients?.map((resident, i) => (
-                    <div key={resident + i} className={styles.item}>
-                        <div className={styles.itemIcon}>
-                            <PersonIcon />
-                        </div>
-
-                        <div>
-                            {resident.name && <h4>{resident.name}</h4>}
-                            <div className={styles.itemPhone}>
-                                <CallIcon
-                                    style={{
-                                        width: "14px",
-                                        height: "14px",
-                                        marginRight: "5px",
-                                    }}
-                                />
-                                {resident.phone}
-                            </div>
-                            {resident.email && (
-                                <div>
-                                    <EmailIcon
-                                        style={{
-                                            width: "14px",
-                                            height: "14px",
-                                            marginRight: "5px",
-                                        }}
-                                    />
-                                    {resident.email}
-                                </div>
-                            )}
-                        </div>
+            {fullAddressTitle && (
+                <>
+                    <div className={styles.topList}>
+                        <h3>{fullAddressTitle}</h3>
+                        <Button>
+                            <PersonAddIcon />
+                        </Button>
                     </div>
-                ))}
-            </div>
+                    <div className={styles.list}>
+                        {residents.clients?.map((resident, i) => (
+                            <div key={resident + i} className={styles.item}>
+                                <div className={styles.information}>
+                                    <div className={styles.itemIcon}>
+                                        <PersonIcon />
+                                    </div>
+
+                                    <div>
+                                        {resident.name && (
+                                            <h4>{resident.name}</h4>
+                                        )}
+                                        <div className={styles.itemPhone}>
+                                            <CallIcon
+                                                style={{
+                                                    width: "14px",
+                                                    height: "14px",
+                                                    marginRight: "5px",
+                                                }}
+                                            />
+                                            {resident.phone}
+                                        </div>
+                                        {resident.email && (
+                                            <div>
+                                                <EmailIcon
+                                                    style={{
+                                                        width: "14px",
+                                                        height: "14px",
+                                                        marginRight: "5px",
+                                                    }}
+                                                />
+                                                {resident.email}
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+
+                                <div className={styles.buttons}>
+                                    <DeleteIcon />
+                                    <div className={styles.buttonsBorder}></div>
+                                    <CreateIcon />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
         </>
     );
 };
